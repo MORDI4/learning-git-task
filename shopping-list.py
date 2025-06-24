5a3818d3e4601f29d14c1bec893bbb256008dabc
Michal Mordarski <mmordarski4@gmail.com>
2025-06-25 00:17:05 +0200

    Add greetings

--- shopping-list.py
+++ shopping-list.py
@@ -9,3 +9,4 @@ for shop, products in shopping_list.items():
     print(f"Idę do {shop.capitalize()}, kupuję tu następujące rzeczy: {[i.capitalize() for i in products]}")
     count += len(products)
 print(f"W sumie kupuję tyle produktów: {count}")
+print("Pozdrawiam serdecznie mojego mentora :)")
